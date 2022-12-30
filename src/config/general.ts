c3ac4aa49e1d201e5a7986b3bd1c1bd4d4bdbb33
Ryan Wong <me@ryanwong.uk>
2022-12-30 02:10:41 +0000

    Completed configuration

--- src/config/general.ts
+++ src/config/general.ts
@@ -1,9 +1,8 @@
 export const generalConfig: GeneralConfigType = {
-
 	// do not touch that
-    __templateVersion: '2.0.0',
+	__templateVersion: '2.0.0',
 
-	name: 'tscord', // the name of your bot
+	name: 'EPGP Bot', // the name of your bot
 	description: '', // the description of your bot
 	defaultLocale: 'en', // default language of the bot, must be a valid locale
 	simpleCommandsPrefix: '!', // default prefix for simple command messages (old way to do commands on discord)
@@ -16,32 +15,30 @@ export const generalConfig: GeneralConfigType = {
 		supportServer: 'https://discord.com/your_invitation_link',
 		gitRemoteRepo: 'https://github.com/barthofu/tscord',
 	},
-	
+
 	automaticUploadImagesToImgur: false, // enable or not the automatic assets upload
 
-	devs: [], // discord IDs of the devs that are working on the bot (you don't have to put the owner's id here)
+	devs: [process.env['BOT_OWNER_ID']], // discord IDs of the devs that are working on the bot (you don't have to put the owner's id here)
 
 	eval: {
 		name: 'bot', // name to trigger the eval command
-		onlyOwner: false // restrict the eval command to the owner only (if not, all the devs can trigger it)
+		onlyOwner: false, // restrict the eval command to the owner only (if not, all the devs can trigger it)
 	},
 
 	// define the bot activities (phrases under its name). Types can be: PLAYING, LISTENING, WATCHING, STREAMING
-    activities: [
+	activities: [
 		{
 			text: 'discord.js v14',
-			type: 'PLAYING'
+			type: 'PLAYING',
 		},
 		{
 			text: 'some knowledge',
-			type: 'STREAMING'
-		}
-	]
-
-}
+			type: 'STREAMING',
+		},
+	],
+};
 
 // global colors
 export const colorsConfig = {
-
-	primary: '#2F3136'
-}
+	primary: '#2F3136',
+};
